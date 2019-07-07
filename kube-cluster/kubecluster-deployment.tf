9a623a6b062da05b3ed64a6df07ffdc88ae4d90a
dilfuza97 <ibaidullaeva1997@gmail.com>
2019-07-07 14:39:55 -0500

    Deleted fsadykov

--- kube-cluster/kubecluster-deployment.tf
+++ kube-cluster/kubecluster-deployment.tf
@@ -1,17 +1,18 @@
 provider "google" {
-  credentials   = "${file("/Users/fsadykov/dilfuz-service-account.json")}"   #GOOGLE_CREDENTIALS to the path of a file containing the credential JSON
-  project       = "${var.project}"
+  credentials = "${file("./dilfuz-service-account.json")}" #GOOGLE_CREDENTIALS to the path of a file containing the credential JSON
+  project     = "${var.project}"
 }
-resource "google_container_cluster" "cluster_dilfuz_com" {
-    name                = "${var.cluster_name}"
-    network             = "default"
-    subnetwork          = "default"
-    zone                = "us-central1-a"
-    min_master_version  = "1.12.8-gke.10"
-    initial_node_count  = "${var.node_count}"
-    project             = "${var.project}"
 
-    node_config {
-      machine_type      = "n1-standard-4"
+resource "google_container_cluster" "cluster_dilfuz_com" {
+  name               = "${var.cluster_name}"
+  network            = "default"
+  subnetwork         = "default"
+  zone               = "us-central1-a"
+  min_master_version = "1.12.8-gke.10"
+  initial_node_count = "${var.node_count}"
+  project            = "${var.project}"
+
+  node_config {
+    machine_type = "n1-standard-4"
   }
 }
